767ec24d4ff256ac373e846a2f19a61aa37903f1
Pablolepz <plepe00@gmail.com>
2020-08-09 16:26:56 -0700

    Added the Python Development Branch
    This includes initial file structure as well as some basic examples.

---
+++ Python/0_Specific_Scenarios/spec_scen.py
@@ -0,0 +1,4 @@
+# PLEASE NOTE:
+
+# THIS IS FOR VERY SPECIFIC PYTHON SCENARIOS
+# If you HAVE NOT READ 1_Basics or the other python files, it very well may be hard for you to use these examples for your benefit.
